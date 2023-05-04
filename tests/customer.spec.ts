120156ed576c909daed4878622f968812aacab21
Andres Convertini <andres.convertini91@gmail.com>
2023-05-04 05:56:35 -0300

    Updating customer information test implemented

--- tests/customer.spec.ts
+++ tests/customer.spec.ts
@@ -6,7 +6,7 @@ describe("Customer Test", () => {
 
     let customer : Customer;
 
-    before(() => {
+    beforeEach(() => {
         customer = new Customer('Mickey', 'DisneyLand', 'mickey@disneyland.com');
     })
 
@@ -22,5 +22,27 @@ describe("Customer Test", () => {
         assert.equal(customer.getName(), 'Mickey', 'Name correct');
         assert.equal(customer.getAdress(), 'DisneyLand', 'Adress correct');
         assert.equal(customer.getEmail(), 'mickey@disneyland.com', 'Email Adress correct');
+    });
+    
+    it("The system should be able to update customer information using valid data.", () => {
+        /**
+         *  The system should be able to update customer information using valid data.
+         *  Scenario:
+         *  1. Given I create a new customer named Mickey Mouse.
+         *  2. When I update Mickey's customer information with Minnie's information.
+         *  3. Then Minnie's information should be stored in the system.
+         */
+
+        // When
+        customer.updateName('Minnie Mouse');
+        customer.updateAdress('Disney World');
+        customer.updateEmail('minnie@disneyworld.com');
+
+        // Then
+        assert.equal(customer.getName(), 'Minnie Mouse', 'Name changed correctly');
+        assert.equal(customer.getAdress(), 'Disney World', 'Addres changed correctly');
+        assert.equal(customer.getEmail(), 'minnie@disneyworld.com', 'Mail changed correctly');
+
+
     });
 });
